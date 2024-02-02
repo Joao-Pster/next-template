ee4fe610bbd1cd4d7f99abcbb5e8cca002c7a891
João (Pster) Freitas <joao@joaopster.com>
2024-02-02 11:17:43 -0300

    Template made by João Pster

--- next.config.js
+++ next.config.js
@@ -1,8 +1,29 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
   },
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src/styles')],
+    prependData: `
+            @import 'common';
+            @import 'fonts';
+            @import 'mixings';
+            @import 'variables';
+            @import 'overrides';
+            @import 'functions';
+        `,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
